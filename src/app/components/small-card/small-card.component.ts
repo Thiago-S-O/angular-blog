4ef3b5b3f812997987f00c2c4a5e0de99002c83f
Thiago-S-O <thiagosoxiii@outlook.com>
2024-02-07 01:39:09 -0300

    Novas atualizações no projeto, para o consumo de um banco de dados

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
+  @Input()
+  photoCover: string = '';
+  @Input()
+  cardTitle: string ='There is no title here!';
+  @Input()
+  Id:string = '0';
 
+  constructor() {}
 }
